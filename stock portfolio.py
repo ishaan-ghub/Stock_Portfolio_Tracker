9cef371c46bed8adfc14a28b5dda0d4455e0534d
Ishaan goel <25122ishaangoel.2020ece@gmail.com>
2025-05-30 15:27:00 +0530

    Removed unwanted comments

--- stock portfolio.py
+++ stock portfolio.py
@@ -168,4 +168,3 @@ if __name__ == "__main__":
             print("Program ended!")
             print("_______________________________________")
             break
-        # Symbol, Quantity, Current_price AS "Current Price", Current_value AS "Current value"
